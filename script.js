56d9705f38d04d2df0d028816da70471f558557c
sorenknippen <114261122+sorenknippen@users.noreply.github.com>
2022-11-02 12:48:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,6 @@
     to your site with Javascript */
 
 console.log("As we live in a state of natural disaster, it is crucial to have you , your family, and your home prepared for hurricane season.  By starting early, you will have a head start stocking up essential items for your home.  It is better to over prepare for a hurricane than to wait until the last minute.  Make your preparation easier by downloading this checklist in ordeer to keep yourself safe during the storm!" 
-            <br><br>"How to prepare for a hurricane?");
+            <br><br>"How to prepare for a hurricane?"
+            "Hurricane Essentials" <br><br>
+            ");
