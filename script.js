d6fdeafb4c582ef21a30b95654b10697c1c2f636
Glitch (hurrican-kane) <none>
2022-11-05 01:48:44 +0000

    😂🐀 Updated with Glitch

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-console.log("Hello, world!");
+let counter = 1;
+
+const goForward = () => {
+  if (counter < 3) {
+    counter++;
+    let idToMatch = "#slide-" + counter;
+    document.querySelector(idToMatch).classList.remove("hidden");
+  }
+};
+
+const goBack = () => {
+  if (counter > 1) {
+    let idToMatch = "#slide-" + counter;
+    document.querySelector(idToMatch).classList.add("hidden");
+    counter--;
+  }
+};
+
+const downloadPNG = () => {
+  let containerToSave = document.querySelector("#checklist");
+  
+  html2canvas(containerToSave, {}).then(
+    (canvas) => {
+      let a = document.createElement("a");
+      a.download = "checklist.png";
+      a.href = canvas.toDataURL("image/png");
+      a.click();
+    }
+  )
+};
+
+const openChecklist = () => {
+  let checklist = document.querySelector("#checklist");
+  checklist.classList.remove("noheight");
+  checklist.classList.add("fullheight");
+}
